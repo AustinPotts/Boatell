4ba309935dfc1e7fcab2c62e494ee5ab2d457ea8
AustinPotts <pottsevolvements@gmail.com>
2019-08-31 11:07:35 -0400

    Schedule Service Alert Controller

--- Boatelligent/View Controller/ScheduleServiceViewController.swift
+++ Boatelligent/View Controller/ScheduleServiceViewController.swift
@@ -10,12 +10,27 @@ import UIKit
 
 class ScheduleServiceViewController: UIViewController {
 
+    
+    @IBOutlet weak var serviceDatePicker: UIDatePicker!
+    
+  
+    @IBAction func scheduleServiceTapped(_ sender: Any) {
+        showAlert()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+    
+    private func showAlert(){
+        let alert = UIAlertController(title: "Your boat service has been scheduled.", message: "Thank You! An email will be sent to you with more information.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Okay", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+        
+    }
 
     /*
     // MARK: - Navigation
