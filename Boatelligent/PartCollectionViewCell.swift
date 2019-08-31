d0ba47b6071a58a201a75147f5791b741cccf28a
AustinPotts <pottsevolvements@gmail.com>
2019-08-30 20:25:41 -0400

    First Commit

---
+++ Boatelligent/PartCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PartCollectionViewCell.swift
+//  Boatelligent
+//
+//  Created by Austin Potts on 8/30/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class PartCollectionViewCell: UICollectionViewCell {
+    
+}
