661d9d579148f6a9ded442dee78648ce0a94e2c0
AustinPotts <pottsevolvements@gmail.com>
2019-11-02 00:01:12 -0400

    Configured Custom Collection View
    For Service Selection. Custom Segue needed next.

--- Boatelligent/ServicesViewController.swift
+++ Boatelligent/ServicesViewController.swift
@@ -8,19 +8,54 @@
 
 import UIKit
 
-class ServicesViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+struct CustomData{
+    var title: String
+    var image: UIImage
+    var url: String
+}
+
+class ServicesViewController: UIViewController {
 
     @IBOutlet weak var serviceCollectionView: UICollectionView!
     
      let partController = PartController()
     
+    let data = [
+            CustomData(title: "Idk", image: #imageLiteral(resourceName: "Engine2"), url: "part"),
+            CustomData(title: "Idk", image: #imageLiteral(resourceName: "OilChange"), url: "part"),
+            CustomData(title: "Idk", image: #imageLiteral(resourceName: "Gear2"), url: "part"),
+            CustomData(title: "Idk", image: #imageLiteral(resourceName: "Lights"), url: "part"),
+            CustomData(title: "Idk", image: #imageLiteral(resourceName: "Saw2"), url: "part"),
+            CustomData(title: "Idk", image: #imageLiteral(resourceName: "Anchor"), url: "part"),
+            
+            
+            ]
+    
+   
+    fileprivate let collectionView: UICollectionView = {
+         let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+         let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+         cv.translatesAutoresizingMaskIntoConstraints = false
+         cv.register(CustomCell.self, forCellWithReuseIdentifier: "cell")
+         return cv
+     }()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        serviceCollectionView.delegate = self
-        serviceCollectionView.dataSource = self
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .clear
+        collectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 330).isActive = true
+        collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15).isActive = true
+        collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15).isActive = true
+        collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -40).isActive = true
+        
+//        collectionView.heightAnchor.constraint(equalTo: collectionView.widthAnchor, multiplier: 0.5).isActive = true
+//
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
     
     
@@ -35,39 +70,7 @@ class ServicesViewController: UIViewController, UICollectionViewDelegate, UIColl
     }
     */
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-            return CGSize(width: collectionView.frame.width/2.5, height: collectionView.frame.width/2.5)
-        }
-    
-    
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-              // #warning Incomplete implementation, return the number of sections
-              return 1
-          }
-       
-       func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-           return partController.part.count
-       }
-       
-       func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-           guard  let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PartCell", for: indexPath) as? PartCollectionViewCell else {return UICollectionViewCell()}
-           
-               // Configure the cell
-               let part = partController.part[indexPath.item]
-               cell.parts = part
-               return cell
-       }
-       
-       override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-       if segue.identifier == "ViewCalendarSegue" {
-           guard let indexPath = serviceCollectionView.indexPathsForSelectedItems?.first?.item,
-               let partSelectVC = segue.destination as? PartViewViewController else{return}
-           
-           let selectedPart = partController.part[indexPath]
-           partSelectVC.part = selectedPart
-           
-       }
-    }
+
     
  
     
@@ -76,6 +79,82 @@ class ServicesViewController: UIViewController, UICollectionViewDelegate, UIColl
     
     
 
+}
+
+
+
+
+extension ServicesViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width/2.1, height: collectionView.frame.width/2)
+       }
+       
+     
+       func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+         return partController.part.count
+       }
+        
+      
+        
+        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CustomCell
+            
+                // Configure the cell
+               // let part = partController.part[indexPath.item]
+         cell.data = self.data[indexPath.row]
+                return cell
+        }
+        
+        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ViewCalendarSegue" {
+            guard let indexPath = serviceCollectionView.indexPathsForSelectedItems?.first?.item,
+                let partSelectVC = segue.destination as? PartViewViewController else{return}
+            
+            let selectedPart = partController.part[indexPath]
+            partSelectVC.part = selectedPart
+            
+        }
+     }
+    
+    
+    
+}
+
+class CustomCell: UICollectionViewCell {
+    
+    var data: CustomData? {
+        didSet{
+            guard let data = data else {return}
+            bg.image = data.image
+        }
+    }
+    
+    fileprivate let bg: UIImageView = {
+        let iv = UIImageView()
+        iv.image = #imageLiteral(resourceName: "part2")
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.layer.cornerRadius = 15
+        
+        return iv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(bg)
+        bg.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+        bg.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
+        bg.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
+        bg.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
 }
 
 
--- Boatelligent/TestViewController.swift
+++ Boatelligent/TestViewController.swift
@@ -8,120 +8,120 @@
 
 import UIKit
 
-struct CustomData{
-    var title: String
-    var image: UIImage
-    var url: String
-}
+//struct CustomData{
+//    var title: String
+//    var image: UIImage
+//    var url: String
+//}
+//
+//class TestViewController: UIViewController {
+//
+//    let data = [
+//            CustomData(title: "Idk", image: #imageLiteral(resourceName: "waterfall"), url: "part"),
+//            CustomData(title: "Idk", image: #imageLiteral(resourceName: "part2"), url: "part"),
+//            CustomData(title: "Idk", image: #imageLiteral(resourceName: "part3"), url: "part"),
+//            CustomData(title: "Idk", image: #imageLiteral(resourceName: "part1"), url: "part"),
+//            CustomData(title: "Idk", image: #imageLiteral(resourceName: "part2"), url: "part"),
+//            CustomData(title: "Idk", image: #imageLiteral(resourceName: "part3"), url: "part"),
+//            
+//            
+//            ]
+//    
+//    
+//    fileprivate let collectionView: UICollectionView = {
+//         let layout = UICollectionViewFlowLayout()
+//         layout.scrollDirection = .horizontal
+//         let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+//         cv.translatesAutoresizingMaskIntoConstraints = false
+//         cv.register(CustomCell.self, forCellWithReuseIdentifier: "cell")
+//         return cv
+//     }()
+//    
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        view.addSubview(collectionView)
+//        collectionView.backgroundColor = .black
+//        collectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 40).isActive = true
+//        collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40).isActive = true
+//        collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40).isActive = true
+//        
+//        collectionView.heightAnchor.constraint(equalTo: collectionView.widthAnchor, multiplier: 0.5).isActive = true
+//        
+//        collectionView.delegate = self
+//        collectionView.dataSource = self
+//    }
+//    
+// 
+//    
+//
+//    /*
+//    // MARK: - Navigation
+//
+//    // In a storyboard-based application, you will often want to do a little preparation before navigation
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        // Get the new view controller using segue.destination.
+//        // Pass the selected object to the new view controller.
+//    }
+//    */
+//
+//}
+//
+//extension TestViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+//    
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        return CGSize(width: collectionView.frame.width/2.5, height: collectionView.frame.width/2)
+//    }
+//    
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return data.count
+//    }
+//    
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CustomCell
+//        cell.data = self.data[indexPath.row]
+//        
+//        
+//        return cell
+//    }
+//    
+//    
+//}
 
-class TestViewController: UIViewController {
 
-    let data = [
-            CustomData(title: "Idk", image: #imageLiteral(resourceName: "waterfall"), url: "part"),
-            CustomData(title: "Idk", image: #imageLiteral(resourceName: "part2"), url: "part"),
-            CustomData(title: "Idk", image: #imageLiteral(resourceName: "part3"), url: "part"),
-            CustomData(title: "Idk", image: #imageLiteral(resourceName: "part1"), url: "part"),
-            CustomData(title: "Idk", image: #imageLiteral(resourceName: "part2"), url: "part"),
-            CustomData(title: "Idk", image: #imageLiteral(resourceName: "part3"), url: "part"),
-            
-            
-            ]
-    
-    
-    fileprivate let collectionView: UICollectionView = {
-         let layout = UICollectionViewFlowLayout()
-         layout.scrollDirection = .horizontal
-         let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
-         cv.translatesAutoresizingMaskIntoConstraints = false
-         cv.register(CustomCell.self, forCellWithReuseIdentifier: "cell")
-         return cv
-     }()
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        view.addSubview(collectionView)
-        collectionView.backgroundColor = .black
-        collectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 40).isActive = true
-        collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40).isActive = true
-        collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40).isActive = true
-        
-        collectionView.heightAnchor.constraint(equalTo: collectionView.widthAnchor, multiplier: 0.5).isActive = true
-        
-        collectionView.delegate = self
-        collectionView.dataSource = self
-    }
-    
- 
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
-
-extension TestViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: collectionView.frame.width/2.5, height: collectionView.frame.width/2)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return data.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CustomCell
-        cell.data = self.data[indexPath.row]
-        
-        
-        return cell
-    }
-    
-    
-}
-
-
-class CustomCell: UICollectionViewCell {
-    
-    var data: CustomData? {
-        didSet{
-            guard let data = data else {return}
-            bg.image = data.image
-        }
-    }
-    
-    fileprivate let bg: UIImageView = {
-        let iv = UIImageView()
-        iv.image = #imageLiteral(resourceName: "part2")
-        iv.translatesAutoresizingMaskIntoConstraints = false
-        iv.contentMode = .scaleAspectFill
-        iv.clipsToBounds = true
-        iv.layer.cornerRadius = 15
-        
-        return iv
-    }()
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        contentView.addSubview(bg)
-        bg.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
-        bg.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
-        bg.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
-        bg.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    
-}
+//class CustomCell: UICollectionViewCell {
+//    
+//    var data: CustomData? {
+//        didSet{
+//            guard let data = data else {return}
+//            bg.image = data.image
+//        }
+//    }
+//    
+//    fileprivate let bg: UIImageView = {
+//        let iv = UIImageView()
+//        iv.image = #imageLiteral(resourceName: "part2")
+//        iv.translatesAutoresizingMaskIntoConstraints = false
+//        iv.contentMode = .scaleAspectFill
+//        iv.clipsToBounds = true
+//        iv.layer.cornerRadius = 15
+//        
+//        return iv
+//    }()
+//    
+//    override init(frame: CGRect) {
+//        super.init(frame: frame)
+//        contentView.addSubview(bg)
+//        bg.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+//        bg.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
+//        bg.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
+//        bg.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+//    }
+//    
+//    required init?(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//    
+//    
+//}
