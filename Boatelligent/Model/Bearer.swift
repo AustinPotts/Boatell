5c0a0953ea10dc93c5510183587c5c072c8ef621
AustinPotts <pottsevolvements@gmail.com>
2020-02-01 22:19:50 -0500

    Configured Keychain Wrapper + Login Controller

---
+++ Boatelligent/Model/Bearer.swift
@@ -0,0 +1,14 @@
+//
+//  Bearer.swift
+//  Boatelligent
+//
+//  Created by Austin Potts on 2/1/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Bearer: Codable {
+    let token: String
+    let username: String
+}
---
+++ Boatelligent/Model/UserRepresentation.swift
@@ -0,0 +1,16 @@
+//
+//  UserRepresentation.swift
+//  Boatelligent
+//
+//  Created by Austin Potts on 2/1/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+//MARK: - User Core Data Model Representation
+struct UserRepresentation: Codable {
+    
+    let username: String
+    let password: String
+    
+}
